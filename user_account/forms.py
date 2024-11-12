bb16dacfd979d8a700a6262e04c914e3864943dc
VaragandhiPranay <varagandhipranay999@gmail.com>
2024-11-12 11:43:28 +0530

    form history

--- user_account/forms.py
+++ user_account/forms.py
@@ -11,10 +11,15 @@ class UserDataForm(forms.ModelForm):
             'company_name', 'ip_clearance_level', 'catalog_tasks_id'
         ]
         widgets = {
-            'group': forms.Select(),
-            'role': forms.Select(),
-            'employment_type': forms.Select(),
-            'ip_clearance_level': forms.Select()
+            'name': forms.TextInput(attrs={'autocomplete': 'off'}),
+            'user_id': forms.TextInput(attrs={'autocomplete': 'off'}),
+            'email': forms.EmailInput(attrs={'autocomplete': 'off'}),
+            'group': forms.Select(attrs={'autocomplete': 'off'}),
+            'role': forms.Select(attrs={'autocomplete': 'off'}),
+            'employment_type': forms.Select(attrs={'autocomplete': 'off'}),
+            'company_name': forms.TextInput(attrs={'autocomplete': 'off'}),
+            'ip_clearance_level': forms.Select(attrs={'autocomplete': 'off'}),
+            'catalog_tasks_id': forms.TextInput(attrs={'autocomplete': 'off'}),
         }
 
     def __init__(self, *args, **kwargs):
